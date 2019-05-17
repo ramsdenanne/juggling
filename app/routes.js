d41be3187931fbf8371674c75d3a18e93d93a69f
Anne Ramsden <anne.ramsden@engineering.digital.dwp.gov.uk>
2019-05-17 16:01:24 +0100

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+module.exports = router.post("/juggling-balls-answer",function(request,response){
+
+var jugglingBalls = request.session.data['juggling-balls']
+
+if (jugglingBalls == "3 or more"){
+  response.redirect("/juggling-trick")
+} else {
+  response.redirect("/ineligible")
+}
+
+})
